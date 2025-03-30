5f4d90b86ee9604b665943a332fc91af80d2e847
nolions <pkh_nolions@photons.com.tw>
2025-03-30 11:16:18 +0800

    [feature] espncricinfo News List爬蟲

---
+++ internal/service/crawler/espncricinfo.go
@@ -0,0 +1,99 @@
+package crawler
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"net/http"
+	"sportNews/internal/helper"
+	crawlerModel "sportNews/internal/model/crawler"
+	"sportNews/internal/repository"
+	"strconv"
+	"strings"
+	"xorm.io/xorm"
+)
+
+type EspncricinfoServ struct {
+	Serv   *Serv
+	Source string
+	Domain string
+}
+
+func NewEspncricinfoServ(db *xorm.EngineGroup) *EspncricinfoServ {
+	repo := repository.New(db)
+
+	return &EspncricinfoServ{
+		Serv: &Serv{
+			Repo:   &repo,
+			Source: "espncricinfo",
+		},
+		Domain: "",
+	}
+}
+
+func (s *EspncricinfoServ) Crawler() {
+	s.Serv.CrawlerNewsTemplate(s)
+}
+
+func (s *EspncricinfoServ) Detail(url string) (string, error) {
+	return "", nil
+
+}
+
+func (s *EspncricinfoServ) List(page int) ([]crawlerModel.News, error) {
+	headers := map[string]string{
+		"User-Agent":      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+		"Accept":          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+		"Accept-Language": "en-US,en;q=0.9,zh-TW;q=0.8",
+	}
+
+	queryParams := map[string]string{
+		"page": strconv.Itoa(page),
+	}
+
+	url := "https://www.espncricinfo.com/cricket-news"
+
+	resp, err := helper.SendHTTPRequest(url, http.MethodGet, headers, queryParams)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// 使用 goquery 解析 HTML
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]crawlerModel.News, 0)
+	// 開始解析網頁元素
+	doc.Find("div.ds-border-b.ds-border-line.ds-p-4").Each(func(i int, s *goquery.Selection) {
+		var m crawlerModel.News
+		// 描述
+		desc := s.Find("div").Not("[class]")
+		m.Description = desc.Text()
+
+		// 詳情連結
+		href, exists := s.Find("a").Attr("href")
+		if exists {
+			m.Link = href
+		}
+
+		// 標題
+		title := s.Find("h2.ds-text-title-s.ds-font-bold.ds-text-typo")
+		m.Title = title.Text()
+
+		// 時間
+		time := s.Find("span.ds-text-compact-xs").First()
+		parts := strings.Split(time.Text(), "•")
+		date := parts[0]
+		t, err := helper.ConverseToTimestamp(date, "02-Jan-2006")
+		if err != nil {
+			// TODO
+		} else {
+			m.Time = t
+		}
+
+		data = append(data, m)
+	})
+
+	return data, nil
+}
