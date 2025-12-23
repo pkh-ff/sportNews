4fb03363839d53e82de63a7cd9688da423240662
nolions <lv2410@gmail.com>
2025-12-23 20:53:55 +0800

    [feature] API http status 404 & 405 unit test

---
+++ internal/http/http_test.go
@@ -0,0 +1,14 @@
+package http
+
+import (
+	"os"
+	"sportNews/pkg/log"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	log.InitLogger(false)
+	code := m.Run()
+	log.CloseLogger()
+	os.Exit(code)
+}
